93082a01d36fbe174dc0b5f00485c12cd972fd31
MartinTomeo <martintomeo19@gmail.com>
2026-06-11 00:33:11 -0300

    delecion de codigo comentado

--- frontend/src/app/user/admin/page/admin-users-page/admin-users-page.ts
+++ frontend/src/app/user/admin/page/admin-users-page/admin-users-page.ts
@@ -2,9 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { AdminService } from '../../services/admin.service';
 import { AdminSearchInput } from '../../components/admin-search-input/admin-search-input';
-import { User } from './../../../interfaces/users.interface';
 import { AdminUsersList } from '../../components/admin-users-list/admin-users-list';
-import { catchError, debounceTime, delay, switchMap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 
@@ -43,64 +42,5 @@ export class AdminUsersPage {
 
 }
 
-
-
-/*
-  /// funcion con promise
-
-  usersResource = resource({
-    params: () => ({ query: this.query() }),
-    loader: async( { params } ) => {
-      if(!params.query) return await firstValueFrom(this.adminService.getUsers());
-      return await firstValueFrom(this.adminService.getUsersByName(params.query));
-    }
-  });
-
-
-  ngOnInit() {
-    this.adminService.getUsers().subscribe({
-      next: (data) => {
-        console.log(data);
-        this.users.set(data);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-      complete: () => {
-        console.log('complete');
-      }
-    });
-  }
-
-
-  searchById(query: string) {
-    this.postsService.getUsersById(query).subscribe({
-      next: (data) => {
-        console.log(data);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-      complete: () => {
-        console.log('complete');
-      }
-    });
-  }
-
-  searchByName(query: string) {
-    this.postsService.getUsersByName(query).subscribe({
-      next: (data) => {
-        console.log(data);
-      },
-      error: (err) => {
-        console.log(err);
-        console.log('error re cajetilla');
-      },
-      complete: () => {
-        console.log('complete');
-      }
-    });
-  }
-*/
 
 
